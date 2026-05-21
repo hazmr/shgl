0c9be66f73617e4c1e8b286f967acc6cd3c08c30
hazem <hazemhelal.se@gmail.com>
2026-05-22 01:42:44 +0300

    fix map looping

--- server/src/main/java/org/oji/shgl/mapper/CompanyMapper.java
+++ server/src/main/java/org/oji/shgl/mapper/CompanyMapper.java
@@ -7,7 +7,7 @@ import org.mapstruct.ReportingPolicy;
 import org.oji.shgl.dto.CompanyResponseDto;
 import org.oji.shgl.entity.Company;
 
-@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING)
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING, uses = {JobMapper.class})
 public interface CompanyMapper {
 //    CompanyMapper INSTANCE = Mappers.getMapper(CompanyMapper.class);
     CompanyResponseDto toDto(Company company);
